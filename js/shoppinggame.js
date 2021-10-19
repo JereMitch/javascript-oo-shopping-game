62f572a400dba96c2359d6ce2cd22d4ccdb0d972
JereMitch <jere.mitch8@gmail.com>
2021-10-19 10:11:54 -0600

    round 3

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -20,10 +20,10 @@ let player = {
 }
 
 // Define the Product class - write the Constructor function for Product class here
-function Product(id, name, price, expiryDate){
-    this.id = id,
-    this.name = name,
-    this.price = price, 
+function Product(id, name, price, expiryDate) {
+    this.id = id
+    this.name = name
+    this.price = price
     this.expiryDate = expiryDate
 }
 // Complete the dateDiff function
